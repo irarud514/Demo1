2ab0bff4cb56459bcacf21344ad16b930af25e21
irarud514 <irina.rudnikova34@gmail.com>
2026-03-16 10:37:29 +0300

    Add files via upload

---
+++ demo (1)/demo (1)/demo/src/main/java/com/example/demo/controller/UserInfo.java
@@ -0,0 +1,48 @@
+package com.example.demo.controller;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class UserInfo {
+
+    @JsonProperty("id")
+    private Long id;
+
+    @JsonProperty("firstname")
+    private String firstName;
+
+    @JsonProperty("lastname")
+    private String lastName;
+
+    public UserInfo() {
+    }
+
+    public UserInfo(Long id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
